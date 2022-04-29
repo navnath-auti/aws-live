27efc2cb7986d891eeb12687f8accb299a18a175
navnath-auti <79525404+navnath-auti@users.noreply.github.com>
2022-04-29 21:08:20 +0530

    Update config.py
    changing pass

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.c16rkbe03ung.us-east-1.rds.amazonaws.com"
 customuser = "navnath"
-custompass = "navnath123"
+custompass = "navnathauti"
 customdb = "employee"
 custombucket = "add.emp"
 customregion = "us-east-1"
